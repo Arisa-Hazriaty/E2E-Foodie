78db2594d043f3cb0105172ae652a98d21731e22
sopigp <sopigp@gmail.com>
2021-08-18 20:46:05 +0700

    add tc mencari makanan, tc check-in, tc logout -sopigp

---
+++ Test Suites/Foody.ts
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Foody</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>1756906d-e6e1-4bc9-8065-dce543678867</testSuiteGuid>
+   <testCaseLink>
+      <guid>3b0e930a-c3fa-46d1-962f-2f7e4724a502</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Foody/TC2_MencariMakanan</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>c281b5ae-56cf-4e12-807e-fcb7dd9447bf</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>19e711e0-7c8e-4423-8f30-94c04a6737fc</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f2ccb789-50c3-488b-bf92-6c11b33d6849</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Foody/TC4_Check_in</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f2ffb8ae-6937-4dc0-ac03-ae3e790dc9cd</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Foody/TC5_Logout</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>

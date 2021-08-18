215627e1cddd94d6bca2031ff3c58e1d57c5f505
Nanda <nandariidengen@gmail.com>
2021-08-18 21:49:00 +0700

    Login Negative

---
+++ Test Suites/Login.ts
@@ -0,0 +1,47 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Login</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>61e8316f-9b40-4b2b-b2b7-4644415ce945</testSuiteGuid>
+   <testCaseLink>
+      <guid>6b3c15f7-6280-48d3-a0a0-a620650a65b5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Foody/LoginNegative</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>8bbafa4a-d1fc-4e3e-96b5-4451c810b83d</id>
+         <iterationEntity>
+            <iterationType>RANGE</iterationType>
+            <value>1-1</value>
+         </iterationEntity>
+         <testDataId>Data Files/Login-Negatice</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>8bbafa4a-d1fc-4e3e-96b5-4451c810b83d</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Email</value>
+         <variableId>bba3a2fe-8d02-449a-ae9f-553d6e2c35f7</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>8bbafa4a-d1fc-4e3e-96b5-4451c810b83d</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Password</value>
+         <variableId>8c030bc9-3a8c-450e-ac5f-4899a3aadf77</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>8bbafa4a-d1fc-4e3e-96b5-4451c810b83d</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>ExpectedResult</value>
+         <variableId>957bdc9f-dcca-43c1-8e25-9083934e52c8</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
